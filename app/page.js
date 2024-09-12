40eba4164dece1c899ef52ba1d92999ee7e7836c
Karman1600 <karmanpartapsingh.sidhu@edu.sait.ca>
2024-09-11 20:35:55 -0600

    first commit

---
+++ app/page.js
@@ -0,0 +1,16 @@
+"use client";
+import { auth } from "@/lib/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import Head from "next/head";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+/* Page.js doesn't contain any code because when you open the website,
+it redirects to the page in sign-in folder. */
+
+export default function Page() {
+  return (
+    <div>
+      <Link href={"/sign-in"}>Sign-In</Link>
+    </div>
+  );
+}
