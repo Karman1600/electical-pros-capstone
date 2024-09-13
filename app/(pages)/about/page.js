993f238b81a74d8b007b37ca6d08f55c9bd4e000
Jashangill1 <jashanpreet.singh03@edu.sait.ca>
2024-09-13 08:42:22 -0600

    about us page

--- app/(pages)/about/page.js
+++ app/(pages)/about/page.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 function AboutUs() {
   return (
-    <div>page</div>
-  )
+    <div>
+      Our consulting team leverages our diverse expertise to tackle various
+      engineering challenges. With a robust portfolio of projects spread
+      across Calgary, Edmonton, and other Alberta locations, as well as
+      several projects in British Columbia and Saskatchewan, we have
+      established a track record of success.
+      <p>
+        Our client base spans across multiple sectors including healthcare and
+        treatment centers, residential complexes, commercial businesses,
+        retail outlets, and industrial sites. At the heart of our practice is
+        the belief that integrating cutting-edge materials and technologies
+        with established engineering principles is crucial for developing
+        sustainable and cost-effective solutions tailored for today's built
+        environment.
+      </p>
+    </div>
+  );
 }
 
-export default AboutUs
+export default AboutUs;
