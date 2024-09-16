45182dfb429484638ea434aa14effa3f11ca1f47
mahi0604 <141184262+mahi0604@users.noreply.github.com>
2024-09-16 02:49:19 -0600

    Update page.js

--- app/(pages)/ContactUs/page.js
+++ app/(pages)/ContactUs/page.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 function ContactUs() {
   return (
-    <div>This is the Contact page</div>
-  )
+    <section id="contact" className="contact">
+      <div className="contact-form">
+        <h2>Contact Us</h2>
+        <div className="container">
+          <form action="action_page.php">
+            <label htmlFor="fname">First Name</label>
+            <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+
+            <label htmlFor="lname">Last Name</label>
+            <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
+
+            <label htmlFor="subject">Subject</label>
+            <textarea id="subject" name="subject" placeholder="Write something.." style={{height: '200px'}}></textarea>
+
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      </div>
+      <footer id="contact">
+        <p>
+          Contact Us: <a href="mailto:info@electrical-pros.com">info@electrical-pros.com</a>
+        </p>
+      </footer>
+    </section>
+  );
 }
 
-export default ContactUs
+export default ContactUs;
