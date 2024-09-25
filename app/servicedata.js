f68ca2a951531467de17e9c2cd6195647a018cf7
mahi0604 <141184262+mahi0604@users.noreply.github.com>
2024-09-25 14:52:29 -0600

    service data added

---
+++ app/servicedata.js
@@ -0,0 +1,33 @@
+
+export const servicedata = [{
+  "id": 1,
+  "service_name": "In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
+}, {
+  "id": 2,
+  "service_name": "Nullam molestie nibh in lectus. Pellentesque at nulla."
+}, {
+  "id": 3,
+  "service_name": "Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh. Quisque id justo sit amet sapien dignissim vestibulum."
+}, {
+  "id": 4,
+  "service_name": "Nunc rhoncus dui vel sem."
+}, {
+  "id": 5,
+  "service_name": "Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci."
+}, {
+  "id": 6,
+  "service_name": "In hac habitasse platea dictumst. Maecenas ut massa quis augue luctus tincidunt."
+}, {
+  "id": 7,
+  "service_name": "Praesent id massa id nisl venenatis lacinia."
+}, {
+  "id": 8,
+  "service_name": "In hac habitasse platea dictumst. Etiam faucibus cursus urna."
+}, {
+  "id": 9,
+  "service_name": "In hac habitasse platea dictumst."
+}, {
+  "id": 10,
+  "service_name": "Donec semper sapien a libero."
+}]
+
